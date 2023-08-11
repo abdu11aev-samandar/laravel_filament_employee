2c37f59dfe7986fc2ac63af965f8b46a4e227a55
samandar <xkas2001@gmail.com>
2023-08-11 16:01:50 +0500

    Created Widget

--- app/Filament/Resources/EmployeeResource/Pages/ListEmployees.php
+++ app/Filament/Resources/EmployeeResource/Pages/ListEmployees.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\EmployeeResource\Pages;
 use App\Filament\Resources\EmployeeResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Contracts\View\View;
 
 class ListEmployees extends ListRecords
 {
@@ -14,6 +15,13 @@ class ListEmployees extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            EmployeeResource\Widgets\EmployeeStatsOverview::class,
         ];
     }
 }
