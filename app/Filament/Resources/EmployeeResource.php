17daa60ad6e120c5e1ae7cb219baa0b44c18c794
samandar <xkas2001@gmail.com>
2023-08-11 10:58:28 +0500

    Filled Employee Resource

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -23,7 +23,22 @@ class EmployeeResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Forms\Components\Card::make([
+                    Forms\Components\Select::make('country_id')
+                        ->relationship('country', 'name')->required(),
+                    Forms\Components\Select::make('state_id')
+                        ->relationship('state', 'name')->required(),
+                    Forms\Components\Select::make('city_id')
+                        ->relationship('city', 'name')->required(),
+                    Forms\Components\Select::make('department_id')
+                        ->relationship('department', 'name')->required(),
+                    Forms\Components\TextInput::make('first_name')->required(),
+                    Forms\Components\TextInput::make('last_name')->required(),
+                    Forms\Components\TextInput::make('address')->required(),
+                    Forms\Components\TextInput::make('zip_code')->required(),
+                    Forms\Components\DatePicker::make('birth_date')->required(),
+                    Forms\Components\DatePicker::make('date_hired')->required(),
+                ])->columns(2)
             ]);
     }
 
@@ -31,26 +46,34 @@ class EmployeeResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('id')->sortable(),
+                Tables\Columns\TextColumn::make('first_name')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('last_name')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('department.name')->sortable(),
+//                Tables\Columns\TextColumn::make('city.name')->sortable(),
+//                Tables\Columns\TextColumn::make('state.name')->sortable(),
+                Tables\Columns\TextColumn::make('date_hired')->date(),
+                Tables\Columns\TextColumn::make('created_at')->dateTime(),
             ])
             ->filters([
-                //
+                Tables\Filters\SelectFilter::make('department')->relationship('department', 'name')
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
-    
+
     public static function getRelations(): array
     {
         return [
             //
         ];
     }
-    
+
     public static function getPages(): array
     {
         return [
@@ -58,5 +81,5 @@ class EmployeeResource extends Resource
             'create' => Pages\CreateEmployee::route('/create'),
             'edit' => Pages\EditEmployee::route('/{record}/edit'),
         ];
-    }    
+    }
 }
