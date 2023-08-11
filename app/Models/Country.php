599336bd335333e898e2f34f31983a51e565edfb
samandar <xkas2001@gmail.com>
2023-08-11 10:03:07 +0500

    Made relationship and filled migration

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Country extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['country_code', 'name'];
+
+    public function employees(): HasMany
+    {
+        return $this->hasMany(Employee::class);
+    }
+
+    public function states(): HasMany
+    {
+        return $this->hasMany(State::class);
+    }
+
 }
--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,8 +4,43 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Employee extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'address',
+        'city_id',
+        'state_id',
+        'country_id',
+        'department_id',
+        'zip_code',
+        'birth_date',
+        'date_hired'
+    ];
+
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function state(): BelongsTo
+    {
+        return $this->belongsTo(State::class);
+    }
+
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function department(): BelongsTo
+    {
+        return $this->belongsTo(Department::class);
+    }
+
 }
