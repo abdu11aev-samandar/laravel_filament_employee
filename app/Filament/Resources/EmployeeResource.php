364903dbb8330ff7303591fbed89304ee0775f39
samandar <xkas2001@gmail.com>
2023-08-11 11:16:19 +0500

    Made Depended Dropdown

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -4,7 +4,10 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\EmployeeResource\Pages;
 use App\Filament\Resources\EmployeeResource\RelationManagers;
+use App\Models\City;
+use App\Models\Country;
 use App\Models\Employee;
+use App\Models\State;
 use Filament\Forms;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
@@ -24,12 +27,37 @@ class EmployeeResource extends Resource
         return $form
             ->schema([
                 Forms\Components\Card::make([
+
                     Forms\Components\Select::make('country_id')
-                        ->relationship('country', 'name')->required(),
+                        ->label('Country')
+                        ->options(Country::all()->pluck('name', 'id')->toArray())
+                        ->reactive()
+                        ->afterStateUpdated(fn(callable $set) => $set('state_id', null)),
+
                     Forms\Components\Select::make('state_id')
-                        ->relationship('state', 'name')->required(),
+                        ->label('State')
+                        ->options(function (callable $get) {
+                            $country = Country::find($get('country_id'));
+                            if (!$country) {
+                                return State::all()->pluck('name', 'id');
+                            }
+                            return $country->states->pluck('name', 'id');
+                        })
+                        ->reactive()
+                        ->afterStateUpdated(fn(callable $set) => $set('city_id', null)),
+
                     Forms\Components\Select::make('city_id')
-                        ->relationship('city', 'name')->required(),
+                        ->label('City')
+                        ->options(function (callable $get) {
+                            $state = State::find($get('state_id'));
+                            if (!$state) {
+                                return City::all()->pluck('name', 'id');
+                            }
+                            return $state->cities->pluck('name', 'id');
+                        })
+                        ->reactive()
+                        ->afterStateUpdated(fn(callable $set) => $set('city_id', null)),
+
                     Forms\Components\Select::make('department_id')
                         ->relationship('department', 'name')->required(),
                     Forms\Components\TextInput::make('first_name')->required(),
--- app/Models/State.php
+++ app/Models/State.php
@@ -23,4 +23,9 @@ class State extends Model
         return $this->hasMany(Employee::class);
     }
 
+    public function cities(): HasMany
+    {
+        return $this->hasMany(City::class);
+    }
+
 }
