cb6d1ff7ae6543d016d088ce78ef1ad82853c114
samandar <xkas2001@gmail.com>
2023-08-11 17:05:13 +0500

    Created Employee api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Filament\Resources\EmployeeResource;
+use App\Models\Employee;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/employees', function () {
+    $employees = Employee::orderBy('last_name', 'ASC')->get();
+
+    return EmployeeResource::collection($employees);
 });
